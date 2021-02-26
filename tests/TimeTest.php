ac8b8f73955aba50dac9bac5de76314cd751ee04
julien <juliend2@gmail.com>
2021-02-25 18:13:02 -0800

    tests

--- tests/TimeTest.php
+++ tests/TimeTest.php
@@ -4,14 +4,24 @@ use PHPUnit\Framework\TestCase;
 
 final class TimeTest extends TestCase
 {
-	public function testTimeIsTwoDaysEarlier(): void
-	{
-	  $t1 = Time::fromString('2 days ago');
+  public function testTimeIsTwoDaysEarlier(): void
+  {
+    $t1 = Time::fromString('2 days ago');
     $t2 = Time::fromString('now');
-		$this->assertEquals(
-			2,
-			$t2->daysSince($t1)
-		);
-	}
+    $this->assertEquals(
+      2,
+      $t2->daysSince($t1)
+    );
+  }
+
+  public function testStringFormatting(): void
+  {
+    $time_string = '2020-01-02 03:52:36';
+    $t = Time::fromString($time_string);
+    $this->assertEquals(
+      $time_string,
+      ''.$t.'' // string context
+    );
+  }
 
 }
